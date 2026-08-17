f5ca5a9fa874798b8c6043c7114032e5421c558f
MITHULESH S <mithulesh.sj@gmail.com>
2026-08-17 10:44:08 +0530

    Time :0 ms (100%) Space : 18 MB (75.3%)

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int oColor = image[sr][sc];
-        if(oColor == color)return image;
-        int n = image.size(), m = image[0].size();
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,-1,0,1};
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty()){
-            auto [x,y] = q.front();
-            q.pop();
-            image[x][y] = color;
-            for(int i = 0;i<4;i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx<0||ny<0||nx>=n||ny>=m||image[nx][ny]!=oColor)continue;
-                q.push({nx,ny});
-            }
+    vector<int>dx= {-1,0,1,0};
+    vector<int>dy= {0,-1,0,1};
+    int n,m,c;
+    void dfs(vector<vector<int>>&image,int i,int j,int oc){
+        if(i<0||j<0||i>=n|j>=m||image[i][j]!=oc)return;
+        image[i][j]=c;
+        for(int k = 0;k<4;k++){
+            dfs(image,i+dx[k],j+dy[k],oc);
         }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int oc = image[sr][sc];
+        if(oc == color)return image;
+        n = image.size(), m = image[0].size();
+        c=color;
+        dfs(image,sr,sc,oc);
         return image;
     }
 };
