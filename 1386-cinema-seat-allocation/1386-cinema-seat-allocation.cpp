96262721ad3d04dcf68fca548ec8cdc7f48d9a7b
MITHULESH S <mithulesh.sj@gmail.com>
2026-08-19 22:23:51 +0530

    Time :458 ms (5.07%) Space : 80.5 MB (25.85%)

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        sort(reservedSeats.begin(),reservedSeats.end());
+        int ans = 0;
+        int r = 0;
+        map<int,vector<int>>mp;
+        for(vector<int>row:reservedSeats){
+            mp[row[0]].push_back(row[1]);
+        }
+        for(auto m:mp){
+            cout<<m.first<<endl;
+            // cout<<(m.first-r)*2<<endl;
+            ans += (m.first-r-1)*2;
+            r = m.first;
+            bool a = true;
+            bool b = true;
+            bool c = true;
+            bool d = true;
+            for(int i:m.second){
+                if(i==2 ||i==3)a =false;
+                else if(i==4 || i==5 )b=false;
+                else if(i==6 || i==7 )c=false;
+                else if(i==8||i==9)d=false;
+            } 
+            if(a && b && c && d){cout<<2<<endl;ans+=2;}
+            else if((a && b)||(c && d)||(b && c)){cout<<1<<endl;ans+=1;}
+        }   
+        if(r<n)
+            ans+=(n-r)*2;
+        return ans;
+    }
+};
