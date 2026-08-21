18cd29cad5a38e4a10041b7da76cf0be0f1dfe16
MITHULESH S <mithulesh.sj@gmail.com>
2026-08-21 21:17:47 +0530

    Time :425 ms (37.76%) Space : 11.9 MB (8.59%)

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int n,m;
-    vector<int>dx = {-1,0,1,0};
-    vector<int>dy = {0,-1,0,1};
-    bool isSafe(int i,int j){
+    vector<int> dx={-1,0,1,0};
+    vector<int> dy={0,-1,0,1};
+    bool check(int i,int j){
         return !(i<0 ||j<0||i>=n||j>=m);
     }
-    bool dfs(vector<vector<char>>& board,vector<vector<bool>>& vis,string &word,int x,int y,int i){
-        vis[x][y] = true;
-        for(int k = 0;k<4;k++){
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if(i+1==word.size())return true;
-            if(isSafe(nx,ny) && !vis[nx][ny] && word[i+1]==board[nx][ny] ){
-                if(dfs(board,vis,word,nx,ny,i+1))return true;
-                vis[nx][ny]=false;
-            }
+    bool dfs(vector<vector<char>>&board,string word,vector<vector<bool>>&vis,int i,int j,int k){
+        if(k==word.size())return true;
+        bool ans = false;
+        for(int x = 0;x<4;x++){
+            int ni = i+dx[x];
+            int nj = j+dy[x];
+        if(check(ni,nj)&& !vis[ni][nj] && board[ni][nj]==word[k]){
+            vis[ni][nj] = true;
+            ans  = ans || dfs(board,word,vis,ni,nj,k+1);
+            vis[ni][nj]=false;
         }
-        return false;
+        }
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
         n = board.size();
         m = board[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(word[0]==board[i][j]){
-                    vector<vector<bool>>vis(n,vector<bool>(m,false));
-                    if(dfs(board,vis,word,i,j,0)){return true;}
+            for(int j =0;j<m;j++){
+                if(board[i][j]==word[0]){
+                    vis[i][j]=true;
+                    if(dfs(board,word,vis,i,j,1))return true;
+                    vis[i][j]=false;
                 }
             }
         }
