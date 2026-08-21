7d1ef5cd27ca9d7124294606f1f23e029edb2eca
MITHULESH S <mithulesh.sj@gmail.com>
2026-08-21 20:28:10 +0530

    Time :3 ms (25.61%) Space : 16.8 MB (51.02%)

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
+    using PIII = pair<int,pair<int,int>>;
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,pair<int,int>>>q;
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2)
-                q.push({0,{i,j}});
+        int ans = 0,org =0;
+        queue<PIII>q;
+        int dx[] ={-1,0,1,0};
+        int dy[]={0,-1,0,1}; 
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({0,{i,j}});
+                }
+                else if(grid[i][j]==1){
+                    org++;
+                }
             }
         }
-        vector<int>dx = {-1,0,1,0};
-        vector<int>dy = {0,-1,0,1};
+
         while(!q.empty()){
-            int t = q.front().first;
+            int t = q.front().first; 
             auto [x,y] = q.front().second;
-            q.pop();
-            ans = max(ans,t);
-            for(int k = 0;k<4;k++){
-                int nx = x+dx[k];
-                int ny = y+dy[k];
-                if(nx<0||ny<0||nx>=n||ny>=m)continue;
+            q.pop(); 
+            for(int i = 0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                ans = max(t,ans);
+                if(nx<0 || ny<0||nx>=n||ny>=m)continue;
                 if(grid[nx][ny]==1){
                     grid[nx][ny]=2;
+                    org--;
                     q.push({t+1,{nx,ny}});
                 }
             }
         }
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]==1)return -1;
-            }
-        }
-        return ans;
+        return org==0?ans:-1;
     }
 };
