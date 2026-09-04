61c13c9ff271c7c4f61643ef66addabe8f4f7e09
MITHULESH S <mithulesh.sj@gmail.com>
2026-09-04 15:31:56 +0530

    Time :0 ms (100%) Space : 54.9 MB (98.81%)

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        int idx = lower_bound(nums.begin(),nums.end(),1) - nums.begin();
-        int val = 1;
-        for(int i = idx; i < n; i++){
-            if(nums[i]==val)val++;
-            else if(nums[i] > val)return val;
+        for(int i = 0; i < n;i++){
+            while(nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
+            }
         }
-        return val;
+        for(int i = 0;i < n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
