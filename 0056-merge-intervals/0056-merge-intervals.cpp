22e1fcdc48a1330dd3e4d4d3895e943cb3e5f60c
MITHULESH S <mithulesh.sj@gmail.com>
2026-09-04 15:11:35 +0530

    Time :8 ms (24.35%) Space : 25.1 MB (5.55%)

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-    void sorting(vector<vector<int>>& intervals){
-        sort(intervals.begin(),intervals.end(),[](const vector<int> &a , const vector<int> &b){
-            if(a[0]==b[0]){
-                return a[1] < b[1];
-            }
-            return a[0] < b[0];
-        });
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sorting(intervals);
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         int st = intervals[0][0],end = intervals[0][1];  
         for(vector<int> inv : intervals ){
