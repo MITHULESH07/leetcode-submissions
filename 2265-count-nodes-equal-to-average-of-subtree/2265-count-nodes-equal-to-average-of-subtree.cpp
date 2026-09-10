ae89e2b8e3fc81174ce8526f64e7dd289e31b8eb
MITHULESH S <mithulesh.sj@gmail.com>
2026-09-10 10:26:54 +0530

    Time :3 ms (91.06%) Space : 15.7 MB (64.72%)

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    using PII = pair<int,int>;
+    int ans = 0;
+    PII checkAvg(TreeNode* root){
+        if(!root)return {0,0};
+        PII left_subtree = checkAvg(root->left);
+        PII right_subtree = checkAvg(root->right);
+        int left_sum = left_subtree.first;
+        int left_nodes = left_subtree.second;
+        int right_sum = right_subtree.first;
+        int right_nodes = right_subtree.second;
+        int total_sum = left_sum + right_sum + root->val;
+        int total_nodes = left_nodes + right_nodes + 1;
+        int avg = (total_sum / total_nodes);
+        if(avg == root->val){
+            ans++;
+        } 
+        return {total_sum , total_nodes};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        checkAvg(root);
+        return ans;
+    }
+};
