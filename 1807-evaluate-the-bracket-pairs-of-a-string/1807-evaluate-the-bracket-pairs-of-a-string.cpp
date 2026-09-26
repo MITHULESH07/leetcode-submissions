68a8a53d3fe1b25b17cff732a5fff08f4815994a
MITHULESH S <mithulesh.sj@gmail.com>
2026-09-26 07:09:43 +0530

    Time :175 ms (13.71%) Space : 120.9 MB (89.72%)

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        string ans ="", word = "";
+        map<string,string>know;
+        bool braces = false;
+        for(int i = 0; i < knowledge.size();i++){
+            know[knowledge[i][0]] = knowledge[i][1]; 
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                braces = true;
+                continue;
+            }
+            if(s[i] == ')' && braces){
+                braces = false;
+                if(know.find(word)!=know.end())
+                    ans += know[word];
+                else 
+                    ans += "?";
+                word = "";
+                continue;
+            }
+            if(braces){
+                word += s[i];
+                continue;
+            }
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
